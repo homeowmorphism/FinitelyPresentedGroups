7cb85393665b05fab341d8d68903c315ae596a8c
Hang Lu Su <hanglu.su@pm.me>
2025-12-09 23:03:57 +0100

    instance of IsFinitelyPresented

--- FinitelyPresentedGroups/FinitelyPresentedGroup.lean
+++ FinitelyPresentedGroups/FinitelyPresentedGroup.lean
@@ -21,16 +21,36 @@ class IsFinitelyPresented (G : Type*) [Group G] : Prop where
   out : ∃ (n : ℕ) (f : (FreeGroup (Fin n)) →* G),
     Function.Surjective f ∧ (MonoidHom.ker f).NormalFG
 
+
+/- class IsFinitelyPresented (G : Type*) [Group G] : Prop where
+  out : ∃ (S: Finset G) (f : (FreeGroup (S)) →* G),
+    Function.Surjective f ∧ (MonoidHom.ker f).NormalFG
+-/
+
 variable (G : Type) [Group G] (g : G)
 
 lemma isFinitelyPresented_iff {G : Type*} [Group G] : IsFinitelyPresented G ↔
     ∃ (S : Finset G) (f : FreeGroup S →* G), Function.Surjective f ∧ (MonoidHom.ker f).NormalFG := by
   sorry
 
-instance [IsFinitelyPresented G] : Group.FG G := by
+instance [h : IsFinitelyPresented G] : Group.FG G := by
+  rw [isFinitelyPresented_iff] at h
   rw [Group.fg_iff_exists_freeGroup_hom_surjective]
+  obtain ⟨S, f, hfsurj, hkernel⟩ := h
+  use S
+  constructor
+  · use f
+    exact hfsurj
+  · exact Finset.finite_toSet S
+
+  lemma fpGroup_is_fgGroup (G: Type*) [Group G] (h: IsFinitelyPresented G) : Group.FG G := by
+  rw [Group.fg_iff_exists_freeGroup_hom_surjective]
+  apply isFinitelyPresented_iff at G
+  --constructor
   sorry
+
 
 lemma isFinitelyPresented_stupid (α : Type) [Finite α] (s : Finset (FreeGroup α)) :
     IsFinitelyPresented ((FreeGroup α) ⧸ normalClosure s) := by
     constructor
+    sorry
