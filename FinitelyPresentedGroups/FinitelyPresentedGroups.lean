ca0b3bfc6bbfbaa0cb502d5a2518986eeca69716
fbarroero <fbarroero@gmail.com>
2025-12-10 19:10:02 +0100

    more structured definition

--- FinitelyPresentedGroups/FinitelyPresentedGroups.lean
+++ FinitelyPresentedGroups/FinitelyPresentedGroups.lean
@@ -1,5 +1,6 @@
 import Mathlib
 
+
 /- variable (α : Type)
 
 #check FreeGroup α
@@ -54,3 +55,80 @@ lemma isFinitelyPresented_stupid (α : Type) [Finite α] (s : Finset (FreeGroup 
     IsFinitelyPresented ((FreeGroup α) ⧸ normalClosure s) := by
     constructor
     sorry
+
+
+
+
+namespace Group
+/-
+variable {α : Type*} [Fintype α]
+
+def FinitelyPresentedGroup [Fintype α] (rels : Finset (FreeGroup α)) :=
+  FreeGroup α ⧸ Subgroup.normalClosure rels
+
+lemma aa (rels : Finset (FreeGroup α)) :
+    PresentedGroup (SetLike.coe rels) = FinitelyPresentedGroup rels := rfl
+ -/
+variable (G : Type*) [Group G] (ι : Type w)
+
+structure Generators where
+  /-- The assignment of each variable to a value in `G`. -/
+  val : ι → G
+  gen : Subgroup.closure (Set.range val) = ⊤
+
+structure Presentation extends Generators G ι where
+  /-- The relations on the generators. -/
+  rels : Set (FreeGroup ι)
+  /-- The normal closure of `rels` is the kernel of the induced map. -/
+  ker_eq : MonoidHom.ker (FreeGroup.lift val) = Subgroup.normalClosure rels
+
+structure FinitePresentation' (ι : Type*) [Fintype ι] extends Presentation G ι where
+  h_fin : Finite rels
+
+structure FinitePresentation (ι : Type*) [Fintype ι] extends Generators G ι where
+  /-- The relations on the generators. -/
+  rels : Finset (FreeGroup ι)
+  /-- The normal closure of `rels` is the kernel of the induced map. -/
+  ker_eq : MonoidHom.ker (FreeGroup.lift val) = Subgroup.normalClosure rels
+
+def expe (ι : Type*) [Fintype ι] : FinitePresentation G ι → Presentation G ι :=
+  fun P ↦
+    { val := P.val
+      gen := P.gen
+      rels := SetLike.coe P.rels
+      ker_eq := P.ker_eq }
+
+
+def isFinitelyPresented' : Prop :=
+  ∃ (ι : Type*) (_ : Fintype ι) (g : Presentation G ι), Finite g.rels
+
+variable (α : Type*)
+
+def presZ : Presentation (Multiplicative ℤ) (Fin 1) :=
+  { val := fun _ ↦ Multiplicative.ofAdd 1
+    gen := by
+
+
+      sorry
+    rels := ∅
+    ker_eq := sorry}
+
+
+section aa
+
+variable {G : Type*} [Group G] (ι : Type w) (P : Presentation G ι)
+
+def isomorph : G ≃* PresentedGroup P.rels where
+  toFun :=
+  sorry
+  invFun := sorry
+  left_inv := sorry
+  right_inv := sorry
+  map_mul' := sorry
+
+
+end aa
+
+
+
+end Group
